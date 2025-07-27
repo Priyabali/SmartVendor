e09fa24a77e8577f5f795dc57a0434cc614973a1
Priya Bali <priyabali2403@email.com>
2025-07-27 19:55:35 +0530

    Initial commit for Firebase Hosting

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBw6KJE_D3MgWjInY5Zcz8fLc1sofRd8PQ",
+  authDomain: "smartvendorauth.firebaseapp.com",
+  projectId: "smartvendorauth",
+  storageBucket: "smartvendorauth.appspot.com",
+  messagingSenderId: "276207215197",
+  appId: "1:276207215197:web:49d4af7fd48b2ed04423b4",
+  measurementId: "G-NJMQDSKQR7"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
